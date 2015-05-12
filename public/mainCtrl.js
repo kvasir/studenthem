2d6389fd8a022fe6fcdfdf230cdd29c44b412dfe
Martin Sandström <maartiin_91@hotmail.com>
2015-05-12 07:35:09 +0200

    lightens the mood

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -2,15 +2,25 @@
 angular.module('myApp', [])
 	.controller('mainCtrl', function($http){
 		var vm = this;
+		
+		vm.header = "Välkommen till STUDENTHEM!!!";
 		vm.addUser = function(userName){	
 			
 			if(!userName) return false;
 			
 			$http.post('/users', { name: userName}).success(function(data) {
 				//check the response code?
+				populateUsers();
+				vm.userName = '';
 			});		
 		}
-		$http.get('/users').success(function(data){
+		var populateUsers = function() {
+			$http.get('/users').success(function(data){
 			vm.users = data;
-		});
+			
+			});
+		}
+		
+		populateUsers();
+		
 	});
