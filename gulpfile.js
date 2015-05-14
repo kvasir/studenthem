4ee6d477de7ecf75e58df9dfcc92de647c2be756
Martin Sandström <maartiin_91@hotmail.com>
2015-05-14 12:13:24 +0200

    Adding watch on main css file

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ gulp.task('serve', function () {
     var server = gls.new('app.js');
     server.start();
     
-    gulp.watch(['public/*.html','public/*.js'], server.notify);
+    gulp.watch(['public/*.html','public/*.js', 'public/*.css'], server.notify);
     gulp.watch(['app.js'], server.start); 
 });
 
