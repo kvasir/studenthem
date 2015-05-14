df25d753fb292d1458b5f0dd9f6f6977b28733c0
John-Philip Johansson <seriema@gmail.com>
2015-05-14 15:08:17 +0200

    Remove dead code.

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -17,25 +17,11 @@ angular.module('myApp', ['uiGmapgoogle-maps'])
 
 	vm.map = { center: { latitude: 59.853631, longitude: 17.646774 }, zoom: 11 };
 
-	vm.addUser = function (userName) {
 
-		if (!userName) return false;
 
-		$http.post('/users', { name: userName }).success(function (data) {
-			//check the response code?
-			populateUsers();
-			vm.userName = '';
-		});
-	};
 
-	var populateUsers = function () {
-		$http.get('/users').success(function (data) {
-			vm.users = data;
 
-		});
-	};
 
-	populateUsers();
 
 	var events = {
 		places_changed: function (searchBox) { }
